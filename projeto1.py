ed4b2c695dac9820970014fe7fe3d2841aa13e64
Gustavo-Pasciano93 <gustavohqpasciano@gmail.com>
2024-01-18 20:56:02 -0300

    projeto inicial

---
+++ projeto1.py
@@ -0,0 +1,7 @@
+print("Seja bem vindo ao gerador de nome de time de futebol de várzea ")
+
+bairro = input("Qual o nome do bairro onde você nasceu? \n")
+
+animal = input(" Qual o seu animal favorito? \n")
+
+print(" Uma sugestão de nome de time é " + animal + " "+ "do" + " " + bairro)
